96ad3a8f73ef95bb1b7d71bcc5c31fa06d8c4b75
ZYNASTHOR <zynasthor@gmail.com>
2024-08-24 02:03:30 -0600

    Alineamiento de tarjetas de servicio
    - Se alinearon las tarjetas de la seccion de servicios

--- src/App.jsx
+++ src/App.jsx
@@ -16,36 +16,17 @@ function App() {
         </a>
       </div>
       <div>
-        <Link to={'/services'} >
+        <Link to={"/services"}>
           <img src={reactLogo} className="logo react" alt="React logo" />
         </Link>
       </div>
 
       <h1>PRO SYSTEMS DGO</h1>
       <CustomButton />
-      {/* <h1>Vite + React</h1>
-      <h3>Vite + React</h3>
-      <h4>Vite + React</h4>
-      <h5>Vite + React</h5>
-      <h6>Vite + React</h6>
-      <div> ASDASD </div>
-      <span> ASDASD </span>
-      <p> XD </p>
-      <text> ss </text> */}
       <div className="card">
-        <p>
-          {" "}
-          Mensaje de <strong className="read-the-docs">
-            {" "}
-            Bienvenida{" "}
-          </strong>{" "}
-        </p>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
